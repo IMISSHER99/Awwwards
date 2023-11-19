cf3ccf4a23cb738a3c67c06e2170d84019e1dd09
mrrobot <mrrobot@kali>
2023-11-19 15:16:12 +0530

    MENU DESIGN COMPLETED NEED TO PERFORM TRANSITIONS

--- src/app/components/menu.tsx
+++ src/app/components/menu.tsx
@@ -1,29 +1,86 @@
+'use client'
 import Link from 'next/link'
+import { useState } from 'react'
 
 interface MenuProps {
 
 }
 
 const Menu: React.FC<MenuProps> = ({ }) => {
+
+    const [toggleMenu, setToggleMenu] = useState<boolean>(false);
+
     return (
         <div className="menu fixed bottom-7 w-full">
             <div className="flex items-center justify-center">
-                <div className="menu-float__wrapper flex p-1.5">
-                    <div className="menu-float__top"></div>
+                <div className={`menu-float__wrapper flex p-1.5 flex-col ${toggleMenu ? 'is-open': ''}`}>
+                    <div className={`menu-float__top w-full mb-1.5  ${toggleMenu ? 'block opacity-1' : 'hidden opacity-0'}`}>
+                        <div className="flex w-full">
+                            <div className="menu-float__col">
+                                <div className='ml-4 mb-6 menu-float__menu-section'>Awards</div>
+                                <ul className="menu-float__menu-nav">
+                                    <li><Link href={"/"}>Winners</Link></li>
+                                    <li><Link href={"/"}>Nominies</Link></li>
+                                    <li><Link href={"/"}>Sites of the Day</Link></li>
+                                    <li><Link href={"/"}>Sites of the Month</Link></li>
+                                </ul>
+                            </div>
+                            <div className="menu-float__col">
+                                <div className='ml-4 mb-6 menu-float__menu-section'>Inspiration</div>
+                                <ul className="menu-float__menu-nav">
+                                    <li><Link href={"/"}>Collection</Link></li>
+                                    <li><Link href={"/"}>Elements</Link></li>
+                                </ul>
+                                <div className='ml-4 mb-6 menu-float__menu-section mt-6'>Academy</div>
+                                <ul className="menu-float__menu-nav">
+                                    <li><Link href={"/"}>Courses</Link></li>
+                                    <li><Link href={"/"}>Masterclasses</Link></li>
+                                </ul>
+                            </div>  
+                            <div className="menu-float__col">
+                                <div className='ml-4 mb-6 menu-float__menu-section'>Directory</div>
+                                <ul className="menu-float__menu-nav">
+                                    <li><Link href={"/"}>Professional</Link></li>
+                                    <li><Link href={"/"}>Internationals</Link></li>
+                                    <li><Link href={"/"}>Freelancers</Link></li>
+                                    <li><Link href={"/"}>Agencies & Studios</Link></li>
+                                </ul>
+                            </div>
+                            <div className="menu-float__col">
+                                <div className='ml-4 mb-6 menu-float__menu-section'>W.</div>
+                                <ul className="menu-float__menu-nav">
+                                    <li><Link href={"/"}>Blog</Link></li>
+                                    <li><Link href={"/"}>Jobs</Link></li>
+                                    <li><Link href={"/"}>Jury</Link></li>
+                                    <li><Link href={"/"}>Products</Link></li>
+                                </ul>
+                            </div>  
+                        </div>
+                    </div>
                     <div className="menu-float__bottom flex justify-center">
-                        <div className="menu-float__layout menu-float__layout--primary">
+                        <div className={`menu-float__layout menu-float__layout--primary ${toggleMenu ? 'w-full' : ''}`}>
                             <div className="flex menu-float__content px-5 items-center gap-1.5">
                                 <Link href={"/"} className='font-bold flex items-center text-white'>w.</Link>
                                 <p className="flex items-center text-white"><strong>home</strong></p>
-                                <div className="menu-float__hamburger">
+                                <div className={`menu-float__hamburger ${toggleMenu ? 'is-open' : ''}`} onClick={() => setToggleMenu(!toggleMenu)}>
                                     <div></div>
                                     <div></div>
                                     <div></div>
                                 </div>
                             </div>
                         </div>
-                        <div className="menu-float__layout menu-float__layout--secondary">
-
+                        <div className={`menu-float__layout menu-float__layout--secondary flex ml-1.5 text-white ${toggleMenu ? 'hidden': ''}`}>
+                            <div className="menu-float__content flex px-5 items-center justify-center gap-1.5">
+                                <p className="capitalize flex items-center ml-3 mr-1.5">
+                                    <strong>home</strong>
+                                </p>
+                                <ul className="flex gap-1.5">
+                                    <li><Link href={"/"} className='flex items-center justify-center w-full menu-float__items'>SOTD</Link></li>
+                                    <li><Link href={"/"} className='flex items-center justify-center w-full menu-float__items'>Nominies</Link></li>
+                                    <li><Link href={"/"} className='flex items-center justify-center w-full menu-float__items'>Directory</Link></li>
+                                    <li><Link href={"/"} className='flex items-center justify-center w-full menu-float__items'>Collections</Link></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
