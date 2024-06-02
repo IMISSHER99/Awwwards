4ede161bed666cc1bb35b776526bb05560209312
Amith <amith@Amiths-Laptop.local>
2024-06-02 18:09:05 +0530

    Created a dummy header component

---
+++ app/_components/header/Header.tsx
@@ -0,0 +1,13 @@
+export type HeaderProps = {
+
+}
+
+const Header:React.FC<HeaderProps> = ({}) => {
+    return (
+        <header>
+            Header Component
+        </header>
+    )
+}
+
+export default Header
