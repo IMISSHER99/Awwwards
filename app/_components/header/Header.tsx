339c8a797117281f5ae2fc5cf329b49335e8c650
imissher <radiantimissher@gmail.com>
2024-06-06 14:37:51 +0530

    Added Mobile Icons

--- app/_components/header/Header.tsx
+++ app/_components/header/Header.tsx
@@ -69,6 +69,7 @@ const Header:React.FC<HeaderProps> = ({}) => {
                 {/* Navigation Dropdown */}
                 <NavigationDropdown display={active}/>
             </div>
+            
         </header>
     )
 }
