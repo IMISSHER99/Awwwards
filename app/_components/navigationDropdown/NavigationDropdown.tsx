e585e27122495cb2c02f8c3037f4fa73f9ec63fc
imissher <radiantimissher@gmail.com>
2024-06-06 14:38:22 +0530

    Created dummy component

---
+++ app/_components/navigationDropdown/NavigationDropdown.tsx
@@ -0,0 +1,9 @@
+export type NavigationDropdownProps = {}
+
+const NavigationDropdown:React.FC<NavigationDropdownProps> = ({}) => {
+    return (
+        <div></div>
+    )
+}
+
+export default NavigationDropdown
