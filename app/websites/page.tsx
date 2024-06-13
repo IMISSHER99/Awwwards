2499719f19d2d4bc395ddb228a52bd99556d367c
imissher <radiantimissher@gmail.com>
2024-06-13 12:19:39 +0530

    Implementing Dropdown in the early stages still not complete yet

--- app/websites/page.tsx
+++ app/websites/page.tsx
@@ -1,8 +1,12 @@
+import Filter from "../_components/filters/Filters"
+
 export type WebsiteProps = {}
 
 const Website:React.FC<WebsiteProps> = ({}) => {
     return (
-        <></>
+        <>
+            <Filter />
+        </>
     )
 }
 
