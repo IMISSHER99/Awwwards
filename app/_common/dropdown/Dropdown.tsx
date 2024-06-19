0e68cebb198e920d78b497ac46477958bd68df97
IMISSHER99 <145341240+IMISSHER99@users.noreply.github.com>
2024-06-19 19:39:15 +0530

    Feature (#35)
    * Created a dummy header component
    
    * Added CSS
    
    * Created github action
    
    * Added on click to the function to change styles for other component
    
    * Changed Order lets see if push works now
    
    * Added Responsive styles
    
    * Added Mobile Icons
    
    * added Navigation Dropdown component
    
    * Changed Font added Navigation Drodown
    
    * Added Drizzle configs, creted .env file. pulled the data from the database instead of hardcoding.
    
    * Addded Menu and website page
    
    * Implementing Dropdown in the early stages still not complete yet
    
    * Dropdown partially completed
    
    * Added Permissions
    
    * Added Worflow Run Functionality
    
    * ADDED PREMISSION FOR WRITING
    
    * Added Permissions (#23)
    
    * Updated Dependencies
    
    * Added Comments
    
    * Removed On Push run
    
    * Updated Dropdown component and also added filters
    
    * Updated Filter Component
    
    * Created Github action to push image to artifcatory. Testing if authentication works
    
    ---------
    
    Co-authored-by: Amith <amith@Amiths-Laptop.local>

--- app/_common/dropdown/Dropdown.tsx
+++ app/_common/dropdown/Dropdown.tsx
@@ -1,17 +1,21 @@
 import { FilterData } from "@/app/_types/filter.types"
 import Link from "next/link"
-import { useEffect, useRef, useState } from "react"
+import { usePathname, useSearchParams, useRouter } from "next/navigation"
+import { useCallback, useEffect, useRef, useState } from "react"
 
 export type DropdownProps = {
     filterData: FilterData[],
-    name: string,
+    filterName: string,
     isOpen: boolean,
     onToggle: () => void
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ filterData, name, isOpen, onToggle }) => {
+const Dropdown: React.FC<DropdownProps> = ({ filterData, filterName, isOpen, onToggle }) => {
     const [height, setHeight] = useState<number | null>(null);
     const dropdownRef = useRef<HTMLUListElement>(null);
+    const router = useRouter()
+    const pathname = usePathname()
+    const searchParams = useSearchParams()
 
     useEffect(() => {
         // Calculate the height when the dropdown opens
@@ -26,10 +30,22 @@ const Dropdown: React.FC<DropdownProps> = ({ filterData, name, isOpen, onToggle 
         }
     }, [isOpen]);
 
+    // Get a new searchParams string by merging the current
+    // searchParams with a provided key/value pair
+    const createQueryString = useCallback(
+        (name: string, value: string) => {
+        const params = new URLSearchParams(searchParams.toString())
+        params.append(name, value)
+    
+        return params.toString()
+        },
+        [searchParams]
+    )
+
     return (
         <>
             <button className="relative flex items-center justify-between min-w-[48px] max-w-[220px] ps-6 pe-6 gap-1.5 bg-white rounded-lg" onClick={onToggle}>
-                <span className="flex items-center justify-between whitespace-nowrap font-normal text-sm">{name}</span>
+                <span className="flex items-center justify-between whitespace-nowrap font-normal text-sm">{filterName}</span>
                 <svg viewBox="0 0 20 20" width={8} className={`transition ${isOpen ? 'rotate-180' : ''}`}>
                     <path d="M1.6,4.1c-0.4,0-0.9,0.2-1.2,0.5c-0.7,0.7-0.7,1.7,0,2.4l8.4,8.4c0.7,0.7,1.7,0.7,2.4,0L19.5,7 c0.7-0.7,0.7-1.7,0-2.4c-0.7-0.7-1.7-0.7-2.4,0L10,11.8L2.8,4.6C2.5,4.3,2.1,4.1,1.6,4.1z"></path>
                 </svg>
@@ -39,7 +55,9 @@ const Dropdown: React.FC<DropdownProps> = ({ filterData, name, isOpen, onToggle 
                             filterData.map(({name, routeTo}, index) => {
                                 return (
                                     <li className="h-fit" key={index}>
-                                        <Link href={routeTo} className="flex items-center h-[40px] px-[30px] py-2 relative transition radio rounded-lg">
+                                        <Link href={
+                                                pathname + '?' + createQueryString(filterName, routeTo)
+                                        } className="flex items-center h-[40px] px-[30px] py-2 relative transition radio rounded-lg">
                                             <span className="text-sm">{name}</span>
                                         </Link>
                                     </li>
--- app/_components/filters/Filters.tsx
+++ app/_components/filters/Filters.tsx
@@ -1,9 +1,9 @@
 'use client'
 import Dropdown from "@/app/_common/dropdown/Dropdown"
-import WithContainer from "../WithContainer"
 import { useEffect, useState } from "react"
 import { getAwardsFilterData } from "@/app/_actions/filters"
 import { Filters } from "@/app/_types/filter.types"
+import Link from "next/link"
 
 export type FilterProps = {}
 
@@ -17,7 +17,12 @@ const Filter:React.FC<FilterProps> = ({}) => {
 
     const [filters, setFilters] = useState<Filters>({
         "awards": [],
-        "category": []
+        "category": [],
+        "tag": [],
+        "technology": [],
+        "font": [],
+        "color": [],
+        "country": []
     });
 
     // Use effect which runs as soon as the component is mounted 
@@ -34,13 +39,28 @@ const Filter:React.FC<FilterProps> = ({}) => {
 
 
     return (
-        <div className="w-full mt-[86px] h-[54px]">
+        <div className="w-full mt-[76px] h-[54px]">
             <div className="flex items-center justify-between bg-grey min-h-[54px] rounded-lg p-1.5">
                 <div className="flex items-center gap-1.5">
-                    <Dropdown filterData={filters.awards} name="Awards" isOpen={openDropdown === 'Awards'} onToggle={() => toggleDropdown('Awards')}/>
-                    <Dropdown filterData={filters.category} name="Category" isOpen={openDropdown === 'Category'}  onToggle={() => toggleDropdown('Category')} />
+                    <Dropdown filterData={filters.awards} filterName="Awards" isOpen={openDropdown === 'Awards'} onToggle={() => toggleDropdown('Awards')}/>
+                    <Dropdown filterData={filters.category} filterName="Category" isOpen={openDropdown === 'Category'}  onToggle={() => toggleDropdown('Category')} />
+                    <Dropdown filterData={filters.tag} filterName="Tag" isOpen={openDropdown === 'Tag'}  onToggle={() => toggleDropdown('Tag')} />
+                    <Dropdown filterData={filters.technology} filterName="Technology" isOpen={openDropdown === 'Technology'}  onToggle={() => toggleDropdown('Technology')} />
+                    <Dropdown filterData={filters.country} filterName="Country" isOpen={openDropdown === 'Country'}  onToggle={() => toggleDropdown('Country')} />
+                    <Dropdown filterData={filters.font} filterName="Font" isOpen={openDropdown === 'Font'}  onToggle={() => toggleDropdown('Font')} />
+                    <Dropdown filterData={filters.color} filterName="Color" isOpen={openDropdown === 'Color'}  onToggle={() => toggleDropdown('Color')} />
                 </div>
                 <div className="flex items-center gap-3">
+                    <ul>
+                        <li className="h-fit">
+                            <Link href={"/websites/test"} className="flex items-center h-[40px] px-[30px] bg-white py-2 relative transition rounded-lg">
+                                <span className="text-sm">Reset Filters</span>
+                                <svg className="ml-2" width="15" height="16" viewBox="0 0 15 16">
+                                    <path d="M9.54947 0H4.63158C2.08842 0 0 2.07158 0 4.63158V8.67368C0 9.04421 0.303158 9.34737 0.673684 9.34737C1.04421 9.34737 1.34737 9.04421 1.34737 8.67368V4.63158C1.34737 2.82947 2.81263 1.36421 4.61474 1.36421H9.51579C11.3516 1.36421 12.8168 2.82947 12.8168 4.63158V8.16842C12.8168 9.97053 11.3516 11.4358 9.54947 11.4358H3.46947L5.50737 9.39789C5.77684 9.12842 5.77684 8.70737 5.50737 8.43789C5.23789 8.16842 4.81684 8.16842 4.54737 8.43789L1.34737 11.6379C1.07789 11.9074 1.07789 12.3284 1.34737 12.5979L4.54737 15.7979C4.68211 15.9326 4.85053 16 5.03579 16C5.20421 16 5.38947 15.9326 5.52421 15.7979C5.79368 15.5284 5.79368 15.1074 5.52421 14.8379L3.46947 12.8H9.54947C12.1095 12.8 14.1811 10.7284 14.1811 8.16842V4.63158C14.1811 2.07158 12.0926 0 9.54947 0Z"></path>
+                                </svg>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
